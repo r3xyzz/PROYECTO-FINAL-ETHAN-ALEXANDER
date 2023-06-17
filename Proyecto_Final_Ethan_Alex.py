3161e5bbb2aeb0b9d02d4f570e33774a31974881
Ethan <127980069+r3xyzz@users.noreply.github.com>
2023-06-17 17:15:57 -0400

    borre el def de variable

--- Proyecto_Final_Ethan_Alex.py
+++ Proyecto_Final_Ethan_Alex.py
@@ -63,13 +63,6 @@ Comprar asiento: Solicita los datos del usuario, luego el usuario escoge un asie
 ======================================================================================================================
 '''
 # Definición de variables globales
-asientos_disponibles = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42]
-asientos_ocupados = []
-precios = {
-    'normal': 78900,
-    'vip': 240000
-}
-datos_pasajeros = {}
 
 
 print("/////////////////////////")
